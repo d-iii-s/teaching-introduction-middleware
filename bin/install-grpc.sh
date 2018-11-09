8edec2c84e49dc8b7391f2170369d03e8fc5bcbe
Petr Tuma <petr.tuma@d3s.mff.cuni.cz>
2018-11-09 08:25:55 +0100

    Further tuning to bump gRPC examples to current version.

--- bin/install-grpc.sh
+++ bin/install-grpc.sh
@@ -15,20 +15,19 @@ echo === Building in ${TMP_DIR:?} ...
 pushd "${TMP_DIR:?}"
 
 echo --- Cloning source repository ...
-git clone -b v1.7.x --depth 1 http://github.com/grpc/grpc .
+# BUGFIX: Even though submodule clones are large, do not attempt shallow submodule cloning.
+git clone -b v1.16.x --depth 1 http://github.com/grpc/grpc .
 git submodule update --init
 
 echo --- Building ...
-# BUGFIX: Specifying some paths explicitly helps bypass possible system protobuf installations.
-make -j 8 prefix="${PREFIX:?}" EXTRA_CFLAGS="-I${PREFIX:?}/include" EXTRA_CXXFLAGS="-I${PREFIX:?}/include" EXTRA_CPPFLAGS="-I${PREFIX:?}/include" EXTRA_LDFLAGS="-L${PREFIX:?}/lib"
+make -j 8 prefix="${PREFIX:?}"
 
 echo --- Installing ...
-# BUGFIX: Specifying some paths explicitly helps bypass possible system protobuf installations.
-make prefix="${PREFIX:?}" EXTRA_CFLAGS="-I${PREFIX:?}/include" EXTRA_CXXFLAGS="-I${PREFIX:?}/include" EXTRA_CPPFLAGS="-I${PREFIX:?}/include" EXTRA_LDFLAGS="-L${PREFIX:?}/lib" install
+make prefix="${PREFIX:?}" install
 # BUGFIX: Standard installation installs incorrect symbolic links that need to be renamed.
 for BASE in grpc++ grpc++_cronet grpc++_error_details grpc++_reflection grpc++_unsecure
 do
-    mv --force "${PREFIX:?}/lib/lib${BASE:?}.so.5" "${PREFIX:?}/lib/lib${BASE:?}.so.1" || true
+    mv --force "${PREFIX:?}/lib/lib${BASE:?}.so.6" "${PREFIX:?}/lib/lib${BASE:?}.so.1" || true
 done
 
 echo --- Cleaning up ...
--- src/grpc-basic-server/python/README.md
+++ src/grpc-basic-server/python/README.md
@@ -4,7 +4,7 @@
 
 ```
 > python3 -m venv .
-> bin/pip install grpcio
+> bin/pip install protobuf grpcio
 > protoc --plugin=protoc-gen-grpc=${HOME:?}/.local/bin/grpc_python_plugin --python_out="." --grpc_out="." example.proto
 > bin/python server.py &
 > bin/python client.py
