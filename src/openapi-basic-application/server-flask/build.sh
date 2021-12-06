bb967b210f0538dc80457e395d7473e1a598a49c
Petr Tuma <petr.tuma@d3s.mff.cuni.cz>
2021-12-06 17:31:36 +0100

    Standard CORS handling for OpenAPI Flask server

--- src/openapi-basic-application/server-flask/build.sh
+++ src/openapi-basic-application/server-flask/build.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 rm -rf ./target
-java -jar ../openapi-generator-cli.jar generate -i ../api.yaml -o ./target -g python-flask
+java -jar ../openapi-generator-cli.jar generate -i ../api.yaml -o ./target -g python-flask -c config.json
--- src/openapi-basic-application/server-flask/controllers/users.py
+++ src/openapi-basic-application/server-flask/controllers/users.py
@@ -4,20 +4,13 @@ from http import HTTPStatus
 from openapi_server.models.user import User
 
 
-# Permissive CORS header to be added to responses.
-# Standard solution would use the flask CORS package
-# but that would require modifying more generated files.
-
-CORS = { 'Access-Control-Allow-Origin' : '*' }
-
-
 user_last_id = 1
 user_data = {}
 
 
 def read_users ():
     global user_data
-    return list (user_data.values ()), HTTPStatus.OK.value, CORS
+    return list (user_data.values ()), HTTPStatus.OK.value
 
 def create_user (body):
     global user_last_id
@@ -27,22 +20,22 @@ def create_user (body):
     body.id = user_last_id
     user_last_id += 1
     user_data [body.id] = body
-    return None, HTTPStatus.CREATED.value, CORS
+    return None, HTTPStatus.CREATED.value
 
 def delete_user (user_id):
     global user_data
     try:
         del user_data [user_id]
-        return None, HTTPStatus.NO_CONTENT.value, CORS
+        return None, HTTPStatus.NO_CONTENT.value
     except KeyError:
-        return None, HTTPStatus.NOT_FOUND.value, CORS
+        return None, HTTPStatus.NOT_FOUND.value
 
 def read_user (user_id):
     global user_data
     try:
-        return user_data [user_id], HTTPStatus.OK.value, CORS
+        return user_data [user_id], HTTPStatus.OK.value
     except KeyError:
-        return None, HTTPStatus.NOT_FOUND.value, CORS
+        return None, HTTPStatus.NOT_FOUND.value
 
 def update_user (body, user_id):
     global user_data
@@ -51,6 +44,6 @@ def update_user (body, user_id):
     body.id = user_id
     if body.id in user_data:
         user_data [body.id] = body
-        return None, HTTPStatus.RESET_CONTENT.value, CORS
+        return None, HTTPStatus.RESET_CONTENT.value
     else:
-        return None, HTTPStatus.NOT_FOUND.value, CORS
+        return None, HTTPStatus.NOT_FOUND.value
